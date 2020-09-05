c027fac4f2329481f0fd7872f0ead6fd2cafb744
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2020-09-05 12:19:35 +0200

    Add specs and keys to breadcrumbs

--- src/main/decide/ui/components/breadcrumbs.cljs
+++ src/main/decide/ui/components/breadcrumbs.cljs
@@ -1,15 +1,28 @@
 (ns decide.ui.components.breadcrumbs
   (:require
+    [cljs.spec.alpha :as s]
+    [clojure.string :as str]
+    [com.fulcrologic.fulcro.dom :as dom]
+    [ghostwheel.core :refer [>defn =>]]
     [material-ui.navigation :as navigation]
     [material-ui.layout :as layout]))
 
-(defn breadcrumb-nav
+(s/def ::label (s/and string? (complement str/blank?)))
+(s/def ::href string?)
+(s/def ::breadcrumb (s/tuple ::label ::href))
+
+(>defn breadcrumb-nav
   [crumbs]
+  [(s/coll-of ::breadcrumb :kind sequential? :distinct true) => dom/element?]
   (layout/box {:pt 1.5 :clone true}
     (navigation/breadcrumbs {:aria-label "breadcrumb"}
       (for [[label href] (butlast crumbs)]
-        (navigation/link {:color "inherit"
+        (navigation/link {:key   [label href]
+                          :color "inherit"
                           :href  href}
           label))
       (let [[label href] (last crumbs)]
-        (navigation/link {:color "textPrimary" :href href} label)))))
+        (navigation/link {:key   [label href]
+                          :color "textPrimary"
+                          :href  href}
+          label)))))
