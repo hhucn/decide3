54057f29bfe8b91f0c17b9cd436afd484a3ff4e4
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2020-09-07 23:30:48 +0200

    Implement live dark-mode

---
+++ src/main/decide/ui/dark_mode.cljs
@@ -0,0 +1,15 @@
+(ns decide.ui.dark-mode)
+
+(def dark-mode-matcher
+  (and
+    (.-matchMedia js/window)
+    (js/window.matchMedia "(prefers-color-scheme: dark)")))
+
+(defn register-dark-mode-listener [f]
+  (when dark-mode-matcher
+    (.addListener dark-mode-matcher f)))
+
+(defn dark-mode?
+  "Checks for prefers-color-scheme: dark. (clj always returns false)"
+  []
+  (and dark-mode-matcher (.-matches dark-mode-matcher)))
--- src/main/decide/ui/root.cljs
+++ src/main/decide/ui/root.cljs
@@ -1,58 +1,52 @@
 (ns decide.ui.root
   (:require
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
+    [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
+    [com.fulcrologic.fulcro.react.hooks :as hooks]
+    [com.fulcrologic.fulcro.routing.dynamic-routing :as dr :refer [defrouter]]
+
+    [decide.models.proposal :as proposal]
+    [decide.ui.components.NewProposal :as new-proposal]
+    [decide.ui.dark-mode :as dark-mode]
+    [decide.ui.login :as login]
+    [decide.ui.main-app :as todo-app]
+    [decide.ui.pages.splash :as splash]
+    [decide.ui.themes :as themes]
+
     [material-ui.utils :as mutils :refer [css-baseline]]
     [material-ui.styles :as styles :refer [prefers-dark?]]
-    [decide.ui.themes :as themes]
-    [decide.ui.components.NewProposal :as new-proposal]
-    [taoensso.timbre :as log]
-    [com.fulcrologic.fulcro.routing.dynamic-routing :as dr :refer [defrouter]]
-    [decide.ui.main-app :as todo-app]
-    [decide.ui.login :as login]
-    [com.fulcrologic.fulcro-css.css-injection :as inj]
-    [clojure.string :as str]
-    [decide.models.proposal :as proposal]
-    [decide.ui.pages.splash :as splash]))
 
+    [taoensso.timbre :as log]))
 
-(def dark-mode-matcher
-  (and
-    (-> js/window .-matchMedia)
-    (-> js/window (.matchMedia "(prefers-color-scheme: dark)"))))
-
-(defn onDarkModeChange [f]
-  (when dark-mode-matcher
-    (.addListener dark-mode-matcher f)))
-
-(defn dark-mode?
-  "Checks for prefers-color-scheme: dark. (clj always returns false)"
-  []
-  (and dark-mode-matcher (.-matches dark-mode-matcher)))
-
-
-(defrouter PageRouter [this {:keys [current-state]}]
+(defrouter PageRouter [_this {:keys [current-state]}]
   {:router-targets [login/LoginPage todo-app/MainApp login/SignUpPage]}
-  (when-not current-state splash/splash)
-  #_(case current-state
-      ; :pending "Loading..."
-      :failed "Failed!"
-      ;; default will be used when the current state isn't yet set
-      (splash/ui-splash-screen nil)))
+  (when-not current-state splash/splash))
 
 (def ui-page-router (comp/factory PageRouter))
 
-(defsc Root [_this {:keys [ui/theme ui/page-router new-proposal-dialog]}]
-  {:query [:ui/theme
-           {:ui/page-router (comp/get-query PageRouter)}
-           {:all-proposals (comp/get-query proposal/Proposal)}
-           {:new-proposal-dialog (comp/get-query new-proposal/NewProposalFormDialog)}]
+(defmutation set-theme [{:keys [theme]}]
+  (action [{:keys [state]}]
+    (swap! state assoc :ui/theme theme)))
+
+(defsc Root [this {:keys [ui/theme ui/page-router new-proposal-dialog]}]
+  {:query      [:ui/theme
+                {:ui/page-router (comp/get-query PageRouter)}
+                {:all-proposals (comp/get-query proposal/Proposal)}
+                {:new-proposal-dialog (comp/get-query new-proposal/NewProposalFormDialog)}]
    :initial-state
-          (fn [_] {:ui/page-router      (comp/get-initial-state PageRouter)
-                   :ui/theme            (if (dark-mode?) :dark :light)
-                   :all-proposals       []
-                   :new-proposal-dialog (comp/get-initial-state new-proposal/NewProposalFormDialog)})}
+               (fn [_] {:ui/page-router      (comp/get-initial-state PageRouter)
+                        :ui/theme            (if (dark-mode/dark-mode?) :dark :light)
+                        :all-proposals       []
+                        :new-proposal-dialog (comp/get-initial-state new-proposal/NewProposalFormDialog)})
+   :use-hooks? true}
+  (hooks/use-effect
+    (fn []
+      (log/debug "Register dark-mode listener")
+      (dark-mode/register-dark-mode-listener #(let [new-theme (if (.-matches %) :dark :light)]
+                                                (comp/transact! (comp/any->app this)
+                                                  [(set-theme {:theme new-theme})]))))
+    [])
   (styles/theme-provider {:theme (themes/get-mui-theme theme)}
     (mutils/css-baseline {})
-    (inj/style-element {:component Root})
     (ui-page-router page-router)
     (new-proposal/ui-new-proposal-form2 new-proposal-dialog)))
