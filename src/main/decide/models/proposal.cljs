dafd88816e1f8b74f1b3d9bebac2b413545c6ea0
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2020-09-18 13:16:22 +0200

    Redesign subtitle for proposal cards

--- src/main/decide/models/proposal.cljs
+++ src/main/decide/models/proposal.cljs
@@ -1,9 +1,9 @@
 (ns decide.models.proposal
   (:require
-    [clojure.string :as str]
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
     [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
     [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]
+    [com.fulcrologic.fulcro.react.hooks :as hooks]
     [com.fulcrologic.fulcro.algorithms.tempid :as tempid]
     [com.fulcrologic.fulcro.data-fetch :as df]
     [decide.routing :as routing]
@@ -52,8 +52,21 @@
         (yesterday? created) (str "gestern um " (.getHours created) ":" (.getMinutes created) " Uhr")
         :else (.toLocaleDateString created)))))
 
+(defn subheader [{:proposal/keys [id created original-author]}]
+  (let [author-name (:profile/name original-author)]
+    (comp/fragment
+      (dom/data {:className "proposal-id"
+                 :value     id}
+        (str "#" (if (tempid/tempid? id) "?" id)))
+      " · "
+      (when author-name
+        (comp/fragment
+          "von " (dom/address (str author-name))))
+      (when created
+        (comp/fragment
+          " am " (dom/time {:dateTime (some-> created .toISOString)} (time-string created)))))))
 
-(defsc Proposal [this {:proposal/keys [id title body opinion created original-author]}]
+(defsc Proposal [this {:proposal/keys [id title body opinion created original-author] :as props}]
   {:query         (fn []
                     [:proposal/id :proposal/title :proposal/body
                      :proposal/pro-votes :proposal/con-votes
@@ -67,44 +80,48 @@
                                :title     title
                                :body      body
                                :pro-votes 0
-                               :con-votes 0})}
-  (layout/box {:width "100%" :clone true}
-    (surfaces/card
-      {:variant :outlined}
-      (surfaces/card-action-area
-        {:href (routing/path->url
-                 (dr/path-to
-                   (comp/registry-key->class 'decide.ui.main-app/MainApp)
-                   (comp/registry-key->class `ProposalPage)
-                   id))}
+                               :con-votes 0})
+   :use-hooks?    true}
+  (let [proposal-href (hooks/use-memo #(routing/path->url
+                                         (dr/path-to
+                                           (comp/registry-key->class 'decide.ui.main-app/MainApp)
+                                           (comp/registry-key->class `ProposalPage)
+                                           id)))]
+    (layout/box {:width "100%" :clone true}
+      (surfaces/card
+        {:variant :outlined}
+
         (layout/box {:pb 0 :clone true}
-          (let [subheader [(dom/data {:class "proposal-id"
-                                      :value id}
-                             (str "#" (if (tempid/tempid? id) "?" id)))
-                           (dom/span
-                             "von " (dom/address (dom/a {:rel "author"} (:profile/name original-author)))
-                             " am " (dom/time {:datetime (some-> created .toISOString)} (time-string created)))]]
-            (surfaces/card-header
-              {:title     title
-               :subheader (apply comp/fragment (interpose " · " subheader))})))
+          (surfaces/card-header
+            {:title     title
+             :subheader (subheader props)}))
         (surfaces/card-content {}
-          (dd/typography {:variant "body2" :color "textSecondary" :component "p"}
-            body)))
-      (surfaces/card-actions {}
-        (inputs/button {:size      :small
-                        :color     (if (pos? opinion) "primary" "default")
-                        :variant   :text
-                        :onClick   #(comp/transact! this [(add-opinion {:proposal/id id
-                                                                        :opinion     (if (pos? opinion) 0 +1)})])
-                        :startIcon (react/createElement ThumbUpAltTwoTone)}
-          "Zustimmen")
-        (inputs/button {:size      :small
-                        :color     (if (neg? opinion) "primary" "default")
-                        :variant   :text
-                        :onClick   #(comp/transact! this [(add-opinion {:proposal/id id
-                                                                        :opinion     (if (neg? opinion) 0 -1)})])
-                        :startIcon (react/createElement ThumbDownAltTwoTone)}
-          "Ablehnen")))))
+          (dd/typography
+            {:component "p"
+             :variant   "body2"
+             :color     "textSecondary"
+             :style     {:whiteSpace "pre-line"}}
+            body))
+        (surfaces/card-actions {}
+
+          (inputs/button {:size      :small
+                          :color     (if (pos? opinion) "primary" "default")
+                          :variant   :text
+                          :onClick   #(comp/transact! this [(add-opinion {:proposal/id id
+                                                                          :opinion     (if (pos? opinion) 0 +1)})])
+                          :startIcon (react/createElement ThumbUpAltTwoTone)}
+            "Zustimmen")
+          (inputs/button {:size      :small
+                          :color     (if (neg? opinion) "primary" "default")
+                          :variant   :text
+                          :onClick   #(comp/transact! this [(add-opinion {:proposal/id id
+                                                                          :opinion     (if (neg? opinion) 0 -1)})])
+                          :startIcon (react/createElement ThumbDownAltTwoTone)}
+            "Ablehnen")
+          (layout/box {:clone true
+                       :style {:marginLeft "auto"}}
+            (inputs/button {:component "a"
+                            :href      proposal-href} "Mehr")))))))
 
 (def ui-proposal (comp/computed-factory Proposal {:keyfn :proposal/id}))
 
