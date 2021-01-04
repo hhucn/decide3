7f1422280909f31914641ca4b111aedd44586263
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2021-01-04 12:53:15 +0100

    Add more helper functions to user ns

--- src/main/decide/models/user.clj
+++ src/main/decide/models/user.clj
@@ -36,6 +36,7 @@
 (s/def ::ident (s/tuple #{::id} ::id))
 (s/def ::email string?)
 (s/def ::password string?)
+(s/def ::encrypted-password string?)
 (s/def ::display-name string?)
 
 (defsc Session [_ _]
@@ -45,7 +46,7 @@
                                       ::id nil}}})
 
 (>defn hash-password [password]
-  [::password => string?]
+  [::password => ::encrypted-password]
   (hs/derive password))
 
 (>defn email-in-db?
@@ -62,13 +63,22 @@
    [d.core/db? ::email => map?]
    (get-by-email db email [::id ::email]))
   ([db email query]
-   [d.core/db? ::email any? => map?]
+   [d.core/db? ::email vector? => map?]
    (d/pull db query [::email email])))
 
 (>defn password-valid? [{:keys [::password]} attempt]
-  [(s/keys :req [::password]) ::password => boolean?]
+  [(s/keys :req [::encrypted-password]) ::password => boolean?]
   (hs/check attempt password))
 
+(>defn new [{::keys [id email password display-name]}]
+  [(s/keys :req [::email ::password] :opt [::id ::display-name])
+   => (s/keys :req [::id ::display-name ::email ::encrypted-password])]
+  (let [id (or id (d.core/squuid))
+        display-name (or display-name email)]
+    {::id id
+     ::email email
+     ::display-name display-name
+     ::password (hash-password password)}))
 
 (defn response-updating-session
   "Uses `mutation-response` as the actual return value for a mutation, but also stores the data into the (cookie-based) session."
@@ -99,11 +109,7 @@
    ::pc/output [:session/valid? ::id :signup/result]}
   (if (email-in-db? @conn email)
     {:errors #{:email-in-use}}
-    (let [id (d.core/squuid)
-          user #:user{::id id
-                      ::email email
-                      ::display-name email
-                      ::password (hash-password password)}
+    (let [{::keys [id] :as user} (new {::email email ::password password})
           tx-report (d/transact conn [user])]
       (response-updating-session
         {:signup/result :success
