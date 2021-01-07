f23658f5c06bd848480fe1658a59ddc14e7a4272
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2021-01-07 18:47:28 +0100

    Use responsive font sizes

--- src/main/decide/ui/theming/themes.cljs
+++ src/main/decide/ui/theming/themes.cljs
@@ -1,23 +1,27 @@
 (ns decide.ui.theming.themes
   (:require [material-ui.styles :refer [create-mui-theme]]
+            ["@material-ui/core/styles" :refer [responsiveFontSizes]]
             [decide.ui.theming.styles :as styles]))
 
 (def shared (js->clj (create-mui-theme {}) :keywordize-keys true))
 (def light-theme (merge
                    shared
-                   {:palette {:type    "light"
+                   {:palette {:type "light"
                               :primary {:main styles/hhu-blue}
                               :secondary {:main "#b3006b"}}}))
 
 (def dark-theme (merge
                   shared
-                  {:palette {:type      "dark"
-                             :primary   {:main "#68b9e9"}
+                  {:palette {:type "dark"
+                             :primary {:main "#68b9e9"}
                              :secondary {:main "#f261a7"}}}))
 
+(defn responsive-font-theme [theme]
+  (responsiveFontSizes (create-mui-theme theme)))
+
 (def compiled-themes
-  {:dark  (create-mui-theme dark-theme)
-   :light (create-mui-theme light-theme)})
+  {:dark (responsive-font-theme dark-theme)
+   :light (responsive-font-theme light-theme)})
 
 (defn get-mui-theme [theme-key]
   (get compiled-themes theme-key (:light compiled-themes)))
