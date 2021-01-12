824d12ba5157f4d0f1c5f84b56bb56767b629c1f
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2021-01-12 12:37:31 +0100

    Change Info logging to debug

--- src/main/decide/routing.cljs
+++ src/main/decide/routing.cljs
@@ -70,12 +70,12 @@
       (zipmap path))))
 
 (defn start-history! [app]
-  (log/info "Start history!")
+  (log/debug "Start history!")
   (reset! history
     (pushy/pushy
       (fn handle-path-change! [path]
         (let [params (extract-params app path)]
-          (log/info "Redirecting: Path:" path " Params:" params)
+          (log/debug "Redirecting: Path:" path " Params:" params)
           (if (routable-path? app path)
             (dr/change-route! app path params)
             ;; change URL and dispatch again
