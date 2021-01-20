75eac2ee8fc626852a5d6d1f09a9cfd05aeb0781
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2021-01-20 16:59:06 +0100

    Implement add-process in backend

--- src/main/decide/models/process.clj
+++ src/main/decide/models/process.clj
@@ -1,13 +1,14 @@
 (ns decide.models.process
   (:require
     [clojure.spec.alpha :as s]
+    [clojure.string :as str]
+    [com.fulcrologic.guardrails.core :refer [>defn => | <-]]
     [com.wsscode.pathom.connect :as pc :refer [defresolver defmutation]]
     [com.wsscode.pathom.core :as p]
     [datahike.api :as d]
     [datahike.core :as d.core]
     [decide.models.authorization :as auth]
     [decide.models.proposal :as proposal]
-    [com.fulcrologic.guardrails.core :refer [>defn => | <-]]
     [taoensso.timbre :as log]))
 
 (def schema
@@ -33,19 +34,22 @@
     :db/cardinality :db.cardinality/one
     :db/valueType :db.type/long}])
 
-(s/def ::slug string?)
-(s/def ::latest-id pos-int?)
+(def slug-pattern #"^[a-z0-9]+(?:-[a-z0-9]+)*$")
+(s/def ::slug (s/and string? (partial re-matches slug-pattern)))
+(s/def ::title (s/and string? (complement str/blank?)))
+(s/def ::description string?)
+(s/def ::latest-id (s/and int? #(<= 0 %)))
 
 (s/def ::ident (s/tuple #{::slug} ::slug))
 
-(>defn get-all-process-slugs [db]
+(>defn get-all-slugs [db]
   [d.core/db? => (s/coll-of ::slug :distinct true)]
   (d/q '[:find [?slug ...]
          :where
          [_ ::slug ?slug]]
     db))
 
-(defn new-nice-id! [conn slug]
+(>defn new-nice-id! [conn slug]
   [d.core/conn? ::slug => ::proposal/nice-id]
   (let [{::keys [latest-id]
          :keys [db/id]}
@@ -56,16 +60,49 @@
     (d/transact conn [[:db/add id ::latest-id (inc latest-id)]])
     (inc latest-id)))
 
+(>defn slug-in-use? [db slug]
+  [d.core/db? ::slug => boolean?]
+  (boolean (d/q '[:find ?e .
+                  :in $ ?slug
+                  :where
+                  [?e :decide.models.process/slug ?slug]]
+             db slug)))
+
+(>defn tx-map [{::keys [title slug description]}]
+  [(s/keys :req [::title ::slug ::description]) => (s/keys :req [::title ::slug ::description ::latest-id])]
+  {::slug slug
+   ::title title
+   ::description description
+   ::proposals []
+   ::latest-id 0})
+
+;; region API
+
 (defresolver resolve-all-processes [{:keys [db]} _]
   {::pc/input #{}
    ::pc/output [{:all-processes [::slug]}]}
   {:all-processes
-   (map #(hash-map ::slug %) (get-all-process-slugs db))})
+   (map #(hash-map ::slug %) (get-all-slugs db))})
 
 (defresolver resolve-process [{:keys [db]} {::keys [slug]}]
   {::pc/input #{::slug}
    ::pc/output [::title ::description]}
   (d/pull db [::title ::description] [::slug slug]))
+
+
+(defmutation add-process [{:keys [conn db AUTH/user-id] :as env} {::keys [title slug description]}]
+  {::pc/params [::title ::slug ::description]
+   ::pc/output [::slug]}
+  (cond
+    user-id nil
+    (s/valid? ::slug title) nil
+    (s/valid? ::title title) nil
+    (s/valid? ::description description) nil
+    (slug-in-use? db slug) nil
+    :else
+    (let [tx-report (d/transact! conn [(tx-map {::slug slug ::title title ::description description})])]
+      {::slug slug
+       ::p/env (assoc env :db (:db-after tx-report))})))
 
 (defresolver resolve-proposals [{:keys [db]} {::keys [slug]}]
   {::pc/input #{::slug}
@@ -120,8 +157,11 @@
 (def resolvers
   [resolve-all-processes
    resolve-process
+   add-process
 
    resolve-proposals
    resolve-proposal-process
    resolve-nice-proposal
    add-proposal])
+
+;; endregion
