7b4d2af3e725933a3470b134311492d74218b137
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2021-02-14 22:08:37 +0100

    Add "needs" to gitlab.ci

--- karma.conf.js
+++ karma.conf.js
@@ -6,12 +6,15 @@ module.exports = function (config) {
         // The file itself
         files: ['ci.js'],
         frameworks: ['cljs-test'],
-        plugins: ['karma-cljs-test', 'karma-chrome-launcher'],
+        plugins: ['karma-cljs-test', 'karma-chrome-launcher', 'karma-junit-reporter'],
         colors: true,
         logLevel: config.LOG_INFO,
         client: {
             args: ["shadow.test.karma.init"],
             singleRun: true
+        },
+        junitReporter: {
+            useBrowserName: false
         }
     })
 };
