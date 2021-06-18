8307ada778a7a0609052bb35a352013af84ae13f
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2021-06-19 01:37:03 +0200

    Implement new event extraction

--- src/main/decide/features/notifications/collect.clj
+++ src/main/decide/features/notifications/collect.clj
@@ -1,25 +1,31 @@
 (ns decide.features.notifications.collect
   (:require
     [clojure.spec.alpha :as s]
+    [com.fulcrologic.guardrails.core :refer [>defn => ? | <-]]
     [datahike.api :as d]
-    [decide.models.argument :as argument]
+    [datahike.core :as d.core]
+    [decide.models.argumentation.database :as argumentation.db]
     [decide.models.process :as process]
-    [decide.models.proposal :as proposal]
-    [decide.server-components.database :refer [conn]])
+    [decide.models.proposal :as proposal])
   (:import (java.time Instant)
            (java.util Date)
            (java.time.temporal ChronoUnit)))
 
+(s/def ::eid pos-int?)
 (s/def :event/what #{:event.type/new-proposal :event.type/new-argument})
 (s/def :event/when inst?)
-(s/def :event/who :decide.models.user/id)
-(s/def :event/tx pos-int?)
-(s/def :event/eid pos-int?)
+(s/def :event/who ::eid)
+(s/def :event/tx ::eid)
+(s/def :event/eid ::eid)
+(s/def :event/proposal ::eid)
+(s/def :event/process ::eid)
 (s/def ::event (s/keys
                  :req [:event/what
                        :event/when
+                       :event/who
                        :event/tx]
-                 :opt [:event/who]))
+                 :opt [:event/proposal
+                       :event/process]))
 
 (defn yesterday []
   (Date/from (.minus (Instant/now) 24 ChronoUnit/HOURS)))
@@ -27,17 +33,16 @@
 (defn minutes-ago [minutes]
   (Date/from (.minus (Instant/now) minutes ChronoUnit/MINUTES)))
 
-(defn to-event [{:keys [entity transaction]}]
-  #:event{:what (if (:decide.models.argument/id entity) :event.type/new-argument :event.type/new-proposal)
+(defn to-event [{:keys [entity tx]}]
+  #:event{:what (if (:argument/id entity) :event.type/new-argument :event.type/new-proposal)
           :eid (:db/id entity)
-          :when (:db/txInstant transaction)
-          :tx (:db/id transaction)})
+          :when (:db/txInstant tx)
+          :tx (:db/id tx)})
 
-(defn new-things [db]
+(defn ^:deprecated new-things [db]
   (->> db
     (d/q '[:find (pull ?e [:db/id
-                           :decide.models.argument/id
-                           :decide.models.argument/content
+                           :argument/id
                            :decide.models.proposal/id
                            ::proposal/title])
            (pull ?tx [:db/id :db/txInstant :db/txUser])
@@ -45,9 +50,38 @@
            :where
            [?e ?a _ ?tx]
            [(contains?
-              #{::argument/id ::proposal/id}
+              #{:argument/id ::proposal/id}
               ?a)]])
     (map to-event)))
+
+(>defn get-new-argument-events [db time-point]
+  [d.core/db? inst? => (s/coll-of ::event)]
+  (d/q
+    '[:find ?what ?when ?e ?tx ?who ?proposal ?process
+      :keys event/what event/when event/eid event/tx event/who event/proposal event/process
+      :in $ $history %
+      :where
+      [(ground :event.type/new-argument) ?what]
+      [$history ?e :argument/id _ ?tx true]                 ; don't include retractions
+      [?tx :db/txInstant ?when]
+      [?e :argument/premise ?premise]
+      [?premise :author ?who]
+      (belongs-to-proposal ?e ?proposal)
+      [?process ::process/proposals ?proposal]]
+    db (d/since db time-point) argumentation.db/argumentation-rules))
+
+
+(>defn get-new-proposal-events [db time-point]
+  [d.core/db? inst? => (s/coll-of ::event)]
+  (d/q '[:find ?what ?when ?e ?tx ?who ?process
+         :keys event/what event/when event/eid event/tx event/who event/process
+         :where
+         [(ground :event.type/new-proposal) ?what]
+         [?e ::proposal/id _ ?tx true]                      ; don't include retractions
+         [?tx :db/txInstant ?when]
+         [?e ::proposal/original-author ?who]
+         [?process ::process/proposals ?e]]
+    (d/since db time-point)))
 
 ;; region enhance-with-slug
 (defmulti enhance-with-slug (fn [_db event] (:event/what event)))
@@ -65,7 +99,15 @@
     (assoc event ::process/slug slug)))
 ;; endregion
 
-(defn events-by-slug [db since]
+(defn all-events [db time-point]
+  [d.core/db? inst? => (s/coll-of ::event :distinct true)]
+  (let [event-extractors [get-new-argument-events get-new-proposal-events]
+        execute! (->> event-extractors
+                   (map (partial comp future))
+                   (apply juxt))]
+    (mapcat deref (execute! db time-point))))
+
+(defn ^:deprecated events-by-slug [db since]
   (->>
     (d/since db since)
     new-things
