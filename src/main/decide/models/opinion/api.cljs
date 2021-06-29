1f9c69d28b50143cd0b114fd96e7e6b519312c95
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2021-06-29 02:50:42 +0200

    Implement single approval voting for the client

--- src/main/decide/models/opinion/api.cljs
+++ src/main/decide/models/opinion/api.cljs
@@ -1,7 +1,22 @@
 (ns decide.models.opinion.api
   (:require
     [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
+    [decide.models.process :as process]
     [decide.models.proposal :as proposal]))
+
+(defn- neutralize-proposal [{::proposal/keys [my-opinion] :as proposal}]
+  (cond-> proposal
+    :always (dissoc ::proposal/my-opinion)
+    ;; This may be overengineered. :-)
+    (pos? my-opinion) (update ::proposal/pro-votes #(- % my-opinion))
+    (neg? my-opinion) (update ::proposal/con-votes #(+ % my-opinion))))
+
+(defn set-neutral [state process-ref]
+  (let [proposal-idents (get-in state (conj process-ref ::process/proposals))]
+    (reduce
+      (fn [state proposal-ident]
+        (update-in state proposal-ident neutralize-proposal))
+      state proposal-idents)))
 
 (defn set* [{::proposal/keys [my-opinion pro-votes] :as proposal} new-opinion]
   (assoc proposal
@@ -11,5 +26,11 @@
 (defmutation add [{::proposal/keys [id]
                    :keys [opinion]}]
   (action [{:keys [state]}]
-    (swap! state update-in [::proposal/id id] set* opinion))
+    (let [current-process-ident (get @state :ui/current-process) ; get this as a parameter? Maybe even the ref?
+          current-process (get-in @state current-process-ident)]
+      (swap! state
+        (fn [state]
+          (cond-> state
+            (process/single-approve? current-process) (set-neutral current-process-ident)
+            :always (update-in [::proposal/id id] set* opinion))))))
   (remote [_] true))
