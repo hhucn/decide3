927ac76ed9070095c8047b33c9b95b396c1569b3
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2021-07-09 15:48:31 +0200

    Fix upper-case of "react" in require

--- src/main/decide/models/user/ui.cljs
+++ src/main/decide/models/user/ui.cljs
@@ -7,7 +7,7 @@
     [material-ui.data-display :as dd]
     [material-ui.layout :as layout]
     [material-ui.styles :as styles]
-    ["React" :as React]))
+    ["react" :as React]))
 
 (def emoji-expr #"^(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?(?:\u200d(?:[^\ud800-\udfff]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?)*")
 
