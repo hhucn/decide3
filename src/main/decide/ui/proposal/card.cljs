3dcb66489b9c78ab30e32bccd06e4697760721b4
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2021-07-14 11:43:36 +0200

    Don't be lazy!

--- src/main/decide/ui/proposal/card.cljs
+++ src/main/decide/ui/proposal/card.cljs
@@ -176,19 +176,23 @@
     #js {:fontSize "small"
          :color (if approved? "primary" "disabled")}))
 
-(defn avatar-group [{:keys [max wrap-overflow]
-                     :or {wrap-overflow identity} :as props} children]
+(defn avatar-group [{:keys [max] :as props} children]
   (dd/avatar-group (update props :max inc)
-    (let [[to-display overflow] (split-at (dec max) children)]
-      (concat
-        (map user.ui/ui-avatar to-display)
-        [(cond
-           (= 1 (count overflow)) (user.ui/ui-avatar (first overflow))
-           (< 1 (count overflow)) (dd/tooltip {:title (list/list {:dense true}
-                                                        (for [{::user/keys [display-name]} overflow]
-                                                          (list/item {:disableGutters true} display-name)))
-                                               :arrow true}
-                                    (dd/avatar {} (str "+" (count overflow)))))]))))
+    (doall
+      (let [[to-display overflow] (split-at (dec max) children)]
+        (concat
+          (map user.ui/ui-avatar to-display)
+          [(cond
+             (= 1 (count overflow)) (user.ui/ui-avatar (first overflow))
+
+             ;; Display "+42" overflow Avatar
+             (< 1 (count overflow))
+             (dd/tooltip {:title (list/list {:dense true}
+                                   (doall
+                                     (for [{::user/keys [display-name]} overflow]
+                                       (list/item {:disableGutters true} display-name))))
+                          :arrow true}
+               (dd/avatar {} (str "+" (count overflow)))))])))))
 
 (defsc ProposalCard [this {::proposal/keys [id title body my-opinion pro-votes parents no-of-arguments opinions]
                            :keys [root/current-session >/subheader]}
