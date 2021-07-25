d622fa32f3181cd7f8af83b19b51d5c81b42f28f
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2021-07-25 14:47:34 +0200

    Add more details to new client side arguments

--- src/main/decide/models/argumentation/api.cljs
+++ src/main/decide/models/argumentation/api.cljs
@@ -1,26 +1,36 @@
 (ns decide.models.argumentation.api
   (:require
     [com.fulcrologic.fulcro.algorithms.merge :as mrg]
-    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
     [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
-    [taoensso.timbre :as log]))
+    [com.fulcrologic.fulcro.raw.components :as rc]))
+
+(def Argument (rc/nc [:argument/id :argument/no-of-arguments :argument/type
+                      {:argument/premise->arguments '...}
+                      {:argument/premise
+                       [:statement/id :statement/content
+                        {:statement/author [:decide.models.user/id]}]}]))
+
+(defn- complete-new-argument [{:keys [state]} argument]
+  (let [user (get-in @state [:root/current-session :user])]
+    (-> argument
+      (assoc :argument/no-of-arguments 0)
+      (update :argument/premise
+        assoc :statement/author user))))
 
 
 (defmutation add-argument-to-statement [{:keys [conclusion]
                                          {premise :argument/premise :as argument} :argument}]
-  (action [{:keys [app component ref]}]
-    (log/info component)
+  (action [{:keys [app ref] :as env}]
     (mrg/merge-component! app
-      component
-      argument
+      Argument
+      (complete-new-argument env argument)
       :append (conj ref :argument/premise->arguments)))
   (remote [_] true))
 
 (defmutation add-argument-to-proposal [{:keys [proposal]
                                         {premise :argument/premise :as argument} :argument}]
-  (action [{:keys [app ref]}]
-    (mrg/merge-component! app
-      (comp/registry-key->class 'decide.models.argumentation.ui/Argument)
-      argument
+  (action [{:keys [app ref] :as env}]
+    (mrg/merge-component! app Argument
+      (complete-new-argument env argument)
       :append (conj ref :decide.models.proposal/positions)))
   (remote [_] true))
