3e8fc5629de697af8c5ded09887ea74f53bb9092
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2021-08-11 18:06:49 +0200

    Refactor some Argument UI stuff
    To tired for a better message

--- src/main/decide/models/argumentation/ui.cljs
+++ src/main/decide/models/argumentation/ui.cljs
@@ -3,6 +3,7 @@
     [clojure.set :as set]
     [com.fulcrologic.fulcro-i18n.i18n :as i18n]
     [com.fulcrologic.fulcro.algorithms.merge :as mrg]
+    [com.fulcrologic.fulcro.application :as app]
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
     [com.fulcrologic.fulcro.data-fetch :as df]
     [com.fulcrologic.fulcro.dom :as dom]
@@ -23,13 +24,28 @@
     [material-ui.layout.grid :as grid]
     [material-ui.surfaces :as surfaces]
     [material-ui.transitions :as transitions]
+    ["@material-ui/icons/AddCircleOutline" :default AddCircleOutline]
+    ["@material-ui/icons/AddComment" :default AddComment]
+    ["@material-ui/icons/Comment" :default Comment]
     ["@material-ui/icons/ExpandLess" :default ExpandLess]
     ["@material-ui/icons/ExpandMore" :default ExpandMore]
-    ["@material-ui/icons/Send" :default Send]
-    ["@material-ui/icons/Close" :default Close]
-    ["@material-ui/icons/AddCircleOutline" :default AddCircleOutline]
-    ["@material-ui/icons/Comment" :default Comment]
-    ["@material-ui/icons/AddComment" :default AddComment]))
+    ["@material-ui/icons/Send" :default Send]))
+
+(defn ui-argument-placeholder [_]
+  (layout/box {:mx 4 :my 1}
+    (grid/container {:spacing 1}
+      (grid/item {:container true :spacing 1}
+        (grid/item {} (skeleton {:variant :circle :width "24px" :height "24px"}))
+        (grid/item {} (skeleton {:width 60})))
+      (grid/item {:xs 12}
+        (skeleton {})
+        (skeleton {:width "60%"})))))
+
+(defn ui-argument-list-placeholder [{:keys [n]}]
+  (grid/container {:spacing 2}
+    (mapv #(grid/item {:key % :xs 12} (ui-argument-placeholder {}))
+      (range (or n 3)))))
+
 
 (defsc StatementAuthor [_ {::user/keys [display-name]}]
   {:query [::user/id ::user/display-name]
@@ -83,7 +99,7 @@
              :multiline true
              :autoFocus true
              :variant :filled
-             :value new-argument
+             :value (or new-argument "")
              :onKeyDown #(when (evt/escape? %) (onClose))
              :onChange #(m/set-string!! this :new-argument :event %)
              :inputProps {:required true}}))
@@ -118,7 +134,6 @@
       (comp/get-initial-state NewArgumentFormCard {:belongs-to belongs-to})
       :replace (conj ref :ui/new-argument-form))))
 
-(declare ui-argument)
 
 (defn type-indicator [type]
   (when (#{:pro :contra} type)
@@ -130,25 +145,45 @@
                          :contra :secondary
                          :default)}))))
 
+(defn argument-header [{:keys [author onClick show-premises?]}]
+  (surfaces/card-header
+    {:avatar (user.ui/chip (set/rename-keys author {::user/display-name :user/display-name
+                                                    ::user/id :user/id}))
+     :action (inputs/icon-button
+               {:size :small
+                :onClick onClick
+                :disabled (nil? onClick)}
+               (dom/create-element (if show-premises? ExpandLess ExpandMore)))}))
+
+(declare ui-argument)
 
 (defsc Argument [this {:argument/keys [id type premise premise->arguments no-of-arguments]
-                       :keys [ui/new-argument-form]}
+                       :keys [ui/new-argument-form] :as props}
                  {:keys [type-feature?]}]
   {:query [:argument/id
            :argument/type
            {:argument/premise (comp/get-query Statement)}
            {:argument/premise->arguments '...}
            :argument/no-of-arguments
-           {:ui/new-argument-form (comp/get-query NewArgumentFormCard)}]
+           {:ui/new-argument-form (comp/get-query NewArgumentFormCard)}
+           [df/marker-table '_]]
    :ident :argument/id
    :use-hooks? true}
   (let [[show-premises? set-show-premises] (hooks/use-state false)
         [new-argument-open? set-new-argument-open] (hooks/use-state false)
-        toggle-list! (hooks/use-callback
-                       (fn toggle-list [_]
-                         (when-not show-premises? (df/refresh! this)) ; only refresh when going to show list
-                         (set-show-premises (not show-premises?)))
-                       [this show-premises?])
+        loading? (df/loading? (get-in props [df/marker-table [::argument-premises id]]))
+        toggle-list! (fn toggle-list [_]
+                       (if loading?
+                         (app/abort! this [::argument-premises id])
+                         ;; only refresh when going to show list
+                         (when-not show-premises?
+                           (df/refresh! this
+                             {:refresh [(comp/get-ident this)]
+                              :parallel true
+                              :marker [::argument-premises id]
+                              :abort-id [::argument-premises id]})))
+                       (set-show-premises (not show-premises?)))
+
         {:statement/keys [author content]} premise]
     (layout/box {:clone true :mr "-1px" :mb "-1px"}
       (surfaces/card {:variant :outlined
@@ -156,18 +191,12 @@
                       :square true
                       :component :article}
 
-        (surfaces/card-header
-          {:avatar (user.ui/chip (set/rename-keys author {::user/display-name :user/display-name
-                                                          ::user/id :user/id}))
-           :action (inputs/icon-button
-                     {:size :small
-                      :onClick toggle-list!
-                      :disabled (zero? no-of-arguments)}
-                     (if show-premises?
-                       (dom/create-element ExpandLess)
-                       (dom/create-element ExpandMore)))})
+        (argument-header {:argument props
+                          :author author
+                          :show-premises? show-premises?
+                          :onClick (when (pos? no-of-arguments) toggle-list!)})
 
-        (layout/box {:ml 3 :px 2 :py 0.5} ; TODO Scale this with viewport size?
+        (layout/box {:ml 3 :px 2 :py 0.5}                   ; TODO Scale this with viewport size?
           (dd/typography {:variant :body2, :color :textPrimary, :component :span}
             (type-indicator type)
             content))
@@ -191,6 +220,7 @@
                  :disabled (not (comp/shared this :logged-in?))
                  :startIcon (dom/create-element AddComment)}
                 (i18n/tr "Add argument")))))
+
         (transitions/collapse {:in (and new-argument-open? new-argument-form)}
           (when new-argument-form
             (surfaces/card-content {}
@@ -206,10 +236,13 @@
                                    (-> {:argument/type (when-not (= :neutral type) type)}
                                      argumentation/make-argument
                                      (assoc :argument/premise new-statement))})])))}))))
-        (when (seq premise->arguments)
-          (transitions/collapse {:in show-premises?}
-            (layout/box {:ml 1}
-              (grid/container {:spacing 1 :direction :column}
+
+        (transitions/collapse {:in show-premises?
+                               :mountOnEnter true}
+          (layout/box {:ml 1}
+            (grid/container {:spacing 1 :direction :column}
+              (if (and loading? (empty? premise->arguments))
+                (ui-argument-list-placeholder {:n (min no-of-arguments 3)})
                 (mapv
                   (fn [argument]
                     (grid/item {:key (:argument/id argument)}
@@ -218,11 +251,7 @@
 
 (def ui-argument (comp/computed-factory Argument {:keyfn :argument/id}))
 
-(def ui-argument-list-placeholder
-  (comp/fragment
-    (grid/item {:xs 12} (skeleton {:variant :rect :height "160px"}))
-    (grid/item {:xs 12} (skeleton {:variant :rect :height "160px"}))
-    (grid/item {:xs 12} (skeleton {:variant :rect :height "160px"}))))
+
 
 
 (defsc ArgumentList [this {::proposal/keys [id positions]
@@ -254,7 +283,7 @@
           (when new-argument-form
             (ui-new-argument-form-card new-argument-form
               {:use-type? type-feature?
-               :onClose #(set-new-argument-open! false)
+               :onClose #(set-new-argument-open! false)     ; THOUGHT This is stupid. Call it onAbort or something..
                :onSave
                (fn [text type]
                  (let [statement (argumentation/make-statement {:statement/content text})]
@@ -266,7 +295,7 @@
                            argumentation/make-argument
                            (assoc :argument/premise statement))})])))}))))
       (if empty-and-loading?
-        ui-argument-list-placeholder
+        (ui-argument-list-placeholder {:n 5})
         (mapv
           (fn [position]
             (grid/item {:key (:argument/id position)
