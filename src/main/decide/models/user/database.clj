ab25b600f30636ca45d938ceee46451fbd6f4d98
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2021-09-10 01:17:52 +0200

    Add tx-gen function for user updates

--- src/main/decide/models/user/database.clj
+++ src/main/decide/models/user/database.clj
@@ -1,7 +1,8 @@
 (ns decide.models.user.database
   (:require
     [datahike.api :as d]
-    [decide.models.user :as user]))
+    [decide.models.user :as user]
+    [decide.server-components.database :as db]))
 
 (defn- user-id-exists? [db user-id]
   (some? (d/q '[:find ?user .
@@ -13,3 +14,11 @@
 (defn get-entity [db user-id]
   (when (user-id-exists? db user-id)
     (d/entity db [::user/id user-id])))
+
+(defn ->update [user]
+  (let [user-id (::user/id user)]
+    (cons
+      (-> user
+        (assoc :db/id [::user/id user-id])
+        (dissoc ::user/id))
+      (db/retract-empty?-tx [::user/id user-id] user))))
