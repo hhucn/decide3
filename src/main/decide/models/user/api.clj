d0e9fe3a1d704d467b8f2d6408de55f230d49b01
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2022-03-11 01:37:42 +0100

    Move user db functions to correct ns

--- src/main/decide/models/user/api.clj
+++ src/main/decide/models/user/api.clj
@@ -1,55 +1,38 @@
 (ns decide.models.user.api
   (:require
     [clojure.spec.alpha :as s]
-    [com.fulcrologic.guardrails.core :refer [>defn >defn- => | ? <-]]
     [com.wsscode.pathom.connect :as pc :refer [defresolver defmutation]]
     [com.wsscode.pathom.core :as p]
-    [datahike.api :as d]
-    [datahike.core :as d.core]
     [decide.models.user :as user]
     [decide.models.user.database :as user.db]
     [decide.server-components.database :as db]
     [taoensso.timbre :as log]))
 
 
-(>defn search-for-user [db search-term]
-  [d.core/db? string? => (s/coll-of (s/keys :req [::user/id]) :distinct true)]
-  (d/q '[:find [(pull ?e [::user/id ::user/display-name]) ...]
-         :in $ ?search-term
-         :where
-         [?e ::user/display-name ?dn]
-         [(clojure.string/lower-case ?dn) ?lc-dn]
-         [(clojure.string/lower-case ?search-term) ?lc-?search-term]
-         [(clojure.string/includes? ?lc-dn ?lc-?search-term)]]
-    db (str search-term)))
-
-
 (defresolver autocomplete-user [{:keys [db] :as env} _]
   {::pc/params [:term :limit]
    ::pc/output [{:autocomplete/users [::user/id ::user/display-name]}]}
-  (let [{:keys [term limit] :or {limit 3}} (-> env :ast :params)
+  (let [{:keys [term limit] :or {limit 3}} (p/params env)
         limit (min limit 5)]
     {:autocomplete/users
-     (take limit (search-for-user db term))}))
+     (take limit (user.db/search db term [::user/id ::user/display-name]))}))
 
 
 (defresolver resolve-nickname-to-id [{:keys [db]} {:user/keys [nickname]}]
   {::pc/output [:user/id]}
-  (d/pull db [[::user/id :as :user/id]] [::user/email nickname]))
+  (user.db/find-by-nickname db nickname [[::user/id :as :user/id]]))
 
 
 (defresolver resolve-public-infos [{:keys [db]} {::user/keys [id]}]
   {::pc/input #{::user/id}
    ::pc/output [::user/display-name :user/nickname]}
-  (d/pull db
-    [::user/display-name [::user/email :as :user/nickname]]
-    [::user/id id]))
+  (user.db/find-by-id db id [::user/display-name [::user/email :as :user/nickname]]))
 
 
 (defresolver resolve-private-infos [{:keys [db AUTH/user-id]} {::user/keys [id]}]
   {::pc/output [:user/email]}
   (if (= user-id id)
-    (d/pull db [:user/email] [::user/id id])
+    (user.db/find-by-id db id [:user/email])
     (do
       (log/warn (format "%s tried to resolve :user/mail for %s" (str user-id) (str id)))
       nil)))
--- src/main/decide/models/user/database.clj
+++ src/main/decide/models/user/database.clj
@@ -1,5 +1,6 @@
 (ns decide.models.user.database
   (:require
+    [clojure.string :as str]
     [datahike.api :as d]
     [decide.models.user :as user]
     [decide.server-components.database :as db]))
@@ -14,3 +15,23 @@
         (assoc :db/id [::user/id user-id])
         (dissoc ::user/id))
       (db/retract-empty?-tx [::user/id user-id] user))))
+
+
+(defn find-by-nickname
+  ([db nickname] (find-by-nickname db nickname ['*]))
+  ([db nickname pattern] (d/pull db pattern [::user/email nickname])))
+
+
+(defn find-by-id
+  ([db user-id] (find-by-nickname db user-id ['*]))
+  ([db user-id pattern] (d/pull db pattern [::user/id user-id])))
+
+
+(defn search [db search-term pattern]
+  (d/q '[:find (pull ?e pattern)
+         :in $ ?search-term pattern
+         :where
+         [?e ::user/display-name ?dn]
+         [(clojure.string/lower-case ?dn) ?lc-dn]
+         [(clojure.string/includes? ?lc-dn ?search-term)]]
+    db (str/lower-case search-term) pattern))
