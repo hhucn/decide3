7eab53b0167129e7eeec68a477a2c5706e1a7360
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2022-05-26 20:26:10 +0200

    Don't add nil keys if user is not logged in.

--- src/main/decide/models/authorization.clj
+++ src/main/decide/models/authorization.clj
@@ -1,10 +1,10 @@
 (ns decide.models.authorization
   (:require
-    [com.fulcrologic.fulcro.components :as comp]
-    [com.fulcrologic.guardrails.core :refer [>defn >defn- => | ? <-]]
-    [com.wsscode.pathom.connect :as pc :refer [defmutation defresolver]]
-    [datahike.api :as d]
-    [decide.models.user :as user]))
+   [com.fulcrologic.fulcro.components :as comp]
+   [com.fulcrologic.guardrails.core :refer [=> >defn ?]]
+   [com.wsscode.pathom.connect :as pc]
+   [datahike.api :as d]
+   [decide.models.user :as user]))
 
 (comp/defsc Session [_ _]
   {:query [{::current-session [:session/valid? ::user/id]}]
@@ -29,13 +29,14 @@
                        ::user/id (::user/id user)}})))
 
 (defn session-wrapper [env]
-  (let [user (get-session-user (:db env) (:ring/request env))]
+  (if-let [user (get-session-user (:db env) (:ring/request env))]
     (assoc env
       :AUTH/user user
       :AUTH/user-id (::user/id user)
       :session (when user
                  {:user {:session/valid? true
-                         ::user/id (::user/id user)}}))))
+                         ::user/id (::user/id user)}}))
+    env))
 
 (defn check-logged-in [{::pc/keys [mutate] :as mutation}]
   (assoc mutation
