46e1770f3b30d2d379befc90e375be17a5e6de0e
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2022-05-31 15:19:32 +0200

    Remove pathom(2) namespace

--- src/dev/user.clj
+++ src/dev/user.clj
@@ -1,25 +1,25 @@
 (ns user
   (:require
-    [analysis :as analysis]
-    [clojure.spec.alpha :as s]
-    [clojure.tools.namespace.repl :as tools-ns :refer [set-refresh-dirs]]
-    [expound.alpha :as expound]
-    ;; this is the top-level dependent component...mount will find the rest via ns requires
-    [decide.server-components.http-server :refer [http-server]]
-    [decide.server-components.email :as email]
-    [decide.features.notifications.notifier :as notifier]
-    [decide.server-components.nrepl :as nrepl]
-    [mount.core :as mount]))
+   [clojure.spec.alpha :as s]
+   [clojure.tools.namespace.repl :as tools-ns :refer [set-refresh-dirs]]
+   [decide.features.notifications.notifier :as notifier]
+   ;; this is the top-level dependent component...mount will find the rest via ns requires
+   [decide.server-components.email :as email]
+   [decide.server-components.http-server]
+   [decide.server-components.nrepl :as nrepl]
+   [expound.alpha :as expound]
+   [mount.core :as mount]))
 
 ;; ==================== SERVER ====================
-(set-refresh-dirs "src/main" "src/dev" "src/test")
+(set-refresh-dirs "src/main" "src/dev" "src/test" "components")
 ;; Change the default output of spec to be more readable
 (alter-var-root #'s/*explain-out* (constantly expound/printer))
 
 (defn start
   "Start the web server"
   []
-  (-> (mount/find-all-states)
+  (->
+    (mount/find-all-states)
     (mount/swap-states
       {#'email/mailer-chan {:start email/dev-mailer
                             :stop #(email/close! email/mailer-chan)}})
